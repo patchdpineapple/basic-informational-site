14c545c87fe423af95b0f862ab4eb6e655209343
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-05-30 21:07:36 +0800

    Server code completed

--- index.js
+++ index.js
@@ -5,28 +5,22 @@ let fs = require("fs");
 http
   .createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = ".";  
-
-    switch(q.pathname) {
-        case "/": 
-            filename += "/index.html";
-            break;
-        case "/about":
-            filename += "/about.html";
-            break;
-        case "/contact-me":
-            filename += "/contact-me.html";
-            break;
-        default: 
-            filename += "/404.html";
-            break;
+    var filename = "." + q.pathname + ".html";
+    if(q.pathname == "/") {
+        filename = "./index.html";
     }
-
-    fs.readFile(filename, function(err, data) {
-        
+    fs.readFile(filename, function(err, data){
+        if(err){
+            return fs.readFile("404.html",function(err,data){
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.write(data);
+                res.end();
+            });
+        } else {
             res.writeHead(200, {"Content-Type": "text/html"});
             res.write(data);
             return res.end();
+        }
     });
   })
   .listen(8080);
